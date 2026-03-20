7ec72bbe2c857d127812698ea974629cfa8610f8
Gabriela Gomes Regis <gabrielaregis08@gmail.com>
2026-03-20 11:37:02 -0300

    pratica 01 finalizada

--- praticas/pratica01/caixa_eletronico.c
+++ praticas/pratica01/caixa_eletronico.c
@@ -1,24 +1,75 @@
 #include <stdio.h>
 
-void cedulas(int n) {
+typedef struct {
+    int n200;
+    int n100;
+    int n50;
+    int n20;
+    int n10;
+    int n5;
+    int n2;
+    int resto;
+} Cedulas;
 
-    int notas200 = n / 200;
-    int notas100 = (n -(notas200 * 200)) / 100;
-    int notas50 = (n - (notas200 *200 + notas100 *100)) / 50;
-    int notas20 = (n - (notas200 *200 + notas100 *100 + notas50 *50)) / 20;
-    int notas10 = (n - (notas200 * 200 + notas100 * 100 + notas50 * 50 + notas20 * 20)) / 10;
-    int notas5 = (n - (notas200 * 200 + notas100 * 100 + notas50 * 50 + notas20 * 20 + notas10 * 10)) / 5;
-    int notas2 = (n - (notas200 * 200 + notas100 * 100 + notas50 * 50 + notas20 * 20 + notas10 * 10 + notas5 * 5)) / 2;
 
+Cedulas qnt_cedulas(int valor) {
+    Cedulas c = {0};  //inicializa struct toda com zero
+
+    if (valor <= 0 || valor > 1000) {
+        c.resto = -1; //indica erro
+        printf("Erro: valor maximo excedido.\n");
+        return c;
+    }
+    
+    c.n200 = valor /200;
+    valor %= 200;
+
+    c.n100 = valor /100;
+    valor %= 100;
+
+    c.n50 = valor /50;
+    valor %= 50;
+
+    c.n20 = valor /20;
+    valor %= 20;
+
+    c.n10 = valor /10;
+    valor %= 10;
+
+    c.n5 = valor /5;
+    valor %= 5;
+
+    c.n2 = valor /2;
+    valor %= 2;
+
+    c.resto = valor;
+
+    return c;
 }
 
 int main() {
 
-    int notas200, notas100, notas50, notas20, notas10, notas5, notas2;
+    printf("Teste -> 774 reais\n");
 
-    cedulas(1262);
-    printf("Cedulas: 200 => %d, 100 => %d, 50 => %d, 20 => %d, 10 => %d, 5 => %d, 2 => %d", 
-                                      notas200, notas100, notas50, notas20, notas10 ,notas5, notas2);
+    Cedulas resultado = qnt_cedulas(774);
+    printf("200: %d\n", resultado.n200);
+    printf("100: %d\n", resultado.n100);
+    printf("50: %d\n", resultado.n50);
+    printf("20: %d\n", resultado.n20);
+    printf("10: %d\n", resultado.n10);
+    printf("5: %d\n", resultado.n5);
+    printf("2: %d\n", resultado.n2);
+
+    printf("Teste -> 2000 reais\n");
+
+    Cedulas result = qnt_cedulas(2000);
+    printf("200: %d\n", result.n200);
+    printf("100: %d\n", result.n100);
+    printf("50: %d\n", result.n50);
+    printf("20: %d\n", result.n20);
+    printf("10: %d\n", result.n10);
+    printf("5: %d\n", result.n5);
+    printf("2: %d\n", result.n2);
 
     return 0;
 }
